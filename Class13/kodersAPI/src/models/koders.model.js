d107a1938c10f4e52a6c27de51cb812607597579
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-05-30 21:44:58 -0600

    koders model

---
+++ Class13/kodersAPI/src/models/koders.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const modelName= 'koders'
+const schema = new mongoose.Schema({firstName:{
+    type: String,
+    required:true,
+    minLength:2,
+    maxLength:100,
+},
+lastName:{
+    type: String,
+    required:false,
+    maxLength:100,
+},
+email:{
+    type: String,
+    required: true,
+    match:/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+},
+birthdate:{
+    type: Date,
+    required:false
+},
+/*generation:{
+    type: Number,
+    min:1,
+    max:100,
+},*/
+password:{
+    type: String,
+    required:true
+},
+
+createdAt:{
+    type:date,
+    default:Date.now
+}
+})
+
+module.exports= mongoose.model('modelName', schema)
+//exporta - > modelo
